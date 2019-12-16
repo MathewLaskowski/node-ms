fecf12e743a28cd9dcdc81351c7bc8f71f55f5bc
mlaskowski <maciejlaskowsky@gmail.com>
2019-12-16 18:06:35 +0100

    add add-to-cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res, next) => {
   })
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId
+  console.log(productId)
+  res.redirect('/cart')
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
