45f82f777d039eaf2be830c69ddcc0efd3fb6472
mlaskowski <maciejlaskowsky@gmail.com>
2019-12-21 19:55:34 +0100

    add product sequlize model

--- models/product.js
+++ models/product.js
@@ -1,49 +1,26 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequlize = require('sequelize');
+const sequlize = require('../util/database');
 
-// const getProductsFromFile = callback => {
-//   fs.readFile(pathFile, (err, fileContent) => {
-//     if (err) {
-//       callback([]);
-//     } else {
-//       callback(JSON.parse(fileContent));
-//     }
-//   })
-// };
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequlize.define('product', {
+  id: {
+    type: Sequlize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequlize.STRING,
+  price: {
+    type: Sequlize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequlize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequlize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id =  ?', [id]);
-  }
-
-  static deleteById(id) {
-    // getProductsFromFile(products => {
-    //   const product = products.find(p => p.id === id);
-    //   const updatedProducts = products.filter(p => p.id !== id);
-    //   fs.writeFile(pathFile, JSON.stringify(updatedProducts), err => {
-    //     if (!err) {
-    //       Cart.deleteProduct(id, product.price);
-    //     }
-    //   })
-    // });
-  }
-};
+module.exports = Product
